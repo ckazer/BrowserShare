54fd1cd91cde2dc294401d4431bfdbe3dd907cf1
Charles Kazer <ckazer1@cs.swarthmore.edu>
2014-11-14 17:15:22 -0500

    WE CONNECTED TO SOMETHING!

--- chromeExtension/background.js
+++ chromeExtension/background.js
@@ -1,3 +1,14 @@
-chrome.browserAction.onClicked.addListener(function{
-  console.log("You are now Browser Sharing!!");
+//var i = 0
+chrome.browserAction.onClicked.addListener(function(tab) {
+//  i += 1
+//  console.log("You are now Browser Sharing!!");
+//  console.log(i)
+    var xhr = new XMLHttpRequest();
+    xhr.open("GET", "http://127.0.0.1:8000", true);
+    xhr.onreadystagechange = function() {
+        if (xhr.readyState == 4) {
+          var resp = xhr.responseText
+        }
+    }
+    xhr.send();
 });
