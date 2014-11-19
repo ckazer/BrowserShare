edd8e50be6c6f88e3e4fa420a41b43228cb07288
Charles Kazer <ckazer1@cs.swarthmore.edu>
2014-11-19 17:03:56 -0500

    Added a lot of options how to contact server in comments. Current implementation pings server and sends along URL of opened tab. Server is able to parse that URL from the message it receives.

--- server/server.py
+++ server/server.py
@@ -2,23 +2,38 @@ from twisted.web.server import Site
 from twisted.web.resource import Resource
 from twisted.internet import reactor
 
+masterURL = ""
+
 class Ping(Resource):
      isLeaf = True
 
      def render_POST(self, request):
          print 'Request is:', request
+
+         global masterURL
+         masterURL = request.uri.split('?')[1][1:] #parses only the url
+         print "masterURL: " + masterURL
+
+         request.setHeader('Access-Control-Allow-Origin', '*')
+         #request.setHeader('Access-Control-Allow-Methods', 'POST')
+         #request.setHeader('Access-Control-Allow-Origin', '*')
+         #request.setHeader('Access-Control-Max-Age', 120)
+         
          return 'Ping from client!'
 
-class Bar(Resource):
+class URL_Update(Resource):
      isLeaf = True
 
      def render_POST(self, request):
          print 'Request is:', request
-         return 'Hello, bar!'
+
+         request.setHeader('Access-Control-Allow-Origin', '*')
+
+         return 'Request to update URL.'
 
 root = Resource()
 root.putChild("ping", Ping())
-root.putChild("bar", Bar())
+root.putChild("URL_update", URL_Update())
 
 factory = Site(root)
 reactor.listenTCP(8880, factory)
